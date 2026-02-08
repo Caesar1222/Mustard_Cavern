6275521c872e08a0169a0be047f92d7087e7d241
Cesar Batrez <124918542+Caesar1222@users.noreply.github.com>
2026-02-08 01:33:29 -0500

    Add support for extracting multiple tar.gz files

--- Extract.bat
+++ Extract.bat
@@ -1,12 +1,20 @@
 @echo off
 set FILE=plutonium.tar.gz
+set FILE1=DS4Windows.tar.gz
 
 if not exist "%FILE%" (
   echo %FILE% not found in this folder.
   pause
   exit /b
 )
+if not exist "%FILE1%" (
+  echo %FILE1% not found in this folder.
+  pause
+  exit /b
+)
 
 tar -xzvf "%FILE%"
+tar -xzvf "%FILE1%"
 echo Done.
+
 pause
